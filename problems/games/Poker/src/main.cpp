fc294b63419cdf9380931234ba6e66d6be73077a
Victor Lai <lai.victor.vl@gmail.com>
2015-12-06 00:38:35 -0500

    Create Poker evaluator

--- problems/games/Poker/src/main.cpp
+++ problems/games/Poker/src/main.cpp
@@ -1,15 +1,29 @@
-#include "poker_card.h"
+#include "poker.h"
 #include <iostream>
+#include <memory>
+
+using namespace Poker;
 
 int main(int argc, char const *argv[])
 {
-    const Poker::Card& aceSpadesString = Poker::Card::Factory::createFromString("Ad");
-    const Poker::Card& aceSpadesValues = Poker::Card::Factory::createFromValues(0, 0);
-    const Poker::Card& aceSpadesCharacters = Poker::Card::Factory::createFromCharacters('A', 'd');
+    Hand hand1(std::vector<Card>{
+        Card{Suit::Diamond, Rank::Ten},
+        Card{Suit::Diamond, Rank::Jack},
+        Card{Suit::Diamond, Rank::Queen},
+        Card{Suit::Diamond, Rank::King},
+        Card{Suit::Diamond, Rank::Ace},
+    });
 
-    std::cout << aceSpadesString << std::endl;
-    std::cout << aceSpadesValues << std::endl;
-    std::cout << aceSpadesCharacters << std::endl;
+    Hand hand2(std::vector<Card>{
+        Card{Suit::Spade, Rank::Ten},
+        Card{Suit::Spade, Rank::Jack},
+        Card{Suit::Spade, Rank::Queen},
+        Card{Suit::Spade, Rank::King},
+        Card{Suit::Spade, Rank::Nine},
+    });
 
+    std::shared_ptr<HandEvaluator> evaluator = std::make_shared<HandEvaluator>();
+
+    std::cout << evaluator->compare(hand1, hand2) << std::endl;
     return 0;
 }
